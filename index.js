66ec28c1c7409791023562cd3268f8a92f26a58e
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-06-09 13:18:53 +0200

    Add eventemitter

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var OSinfo = require('./modules/OSinfo');
+var EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function(instruction){
+	console.log('You wrote:' + instruction + ',trying to run command')
+})
+emitter.on("afterCommand", function(){
+	console.log('Finished command')
+})
 
 process.stdin.setEncoding('utf-8');
-
 process.stdin.on('readable', function(){
 	var input = process.stdin.read();
 	if (input !== null){
 		var instruction = input.toString().trim()
+		emitter.emit('beforeCommand', instruction)
 		switch (instruction) {
 			case '/exit':
 				process.stdout.write('Quitting app!\n');
@@ -23,5 +32,6 @@ process.stdin.on('readable', function(){
 				process.stderr.write('Wrong instruction!\n');
 				break;
 		}
+		emitter.emit('afterCommand')
 	}
 })
